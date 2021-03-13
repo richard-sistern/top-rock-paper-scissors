2737c26f483174b842c1c7cbc0399abb1cf2f974
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-03-13 12:14:00 +0000

    add allowed moves and win conditions

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,27 @@
 const moves = ['Rock', 'Paper', 'Scissors'];
 
+const allowedMoves = {
+    Rock:0,
+    Paper:1,
+    Scissors:2
+};
+console.log(allowedMoves);
+
+// 0 == Rock, 1 == Paper, 2 == Scissors
+// D == Draw, L == Loss, W == Win
+const winConditions = [
+    ['D','L', 'W'],
+    ['W','D', 'L'],
+    ['L','W', 'D']
+];
+console.log(winConditions);
+// Paper Vs Rock
+console.log(winConditions[1][0]); // Win
+// Rock Vs Scissors
+console.log(winConditions[0][2]); // Win
+// Rock Vs Paper
+console.log(winConditions[0][1]); // Loss
+
 function computerPlay(moves) {
     return moves[Math.floor(Math.random() * moves.length)];
 }
@@ -10,12 +32,8 @@ const playerSelection = "Rock";
 const computerSelection = computerPlay(moves);
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        return "Draw"
-    }
-    else {
-        return "else"
-    }
+
+    
 }
 
 result = playRound(playerSelection, computerSelection);
