464495fab88bd54ebbb95de32bd83f906d113a75
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-03-19 14:40:06 +0000

    finished task with game and score working

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,4 @@
+// Allowed set of moves
 const moves = ['Rock', 'Paper', 'Scissors'];
 
 // 0 == Rock, 1 == Paper, 2 == Scissors
@@ -13,25 +14,65 @@ const winConditions = [
     ['L','W', 'D']
 ];
 
-function computerPlay(moves) {
-    return Math.floor(Math.random() * moves.length);
+// Ask player for their move
+function playerSelection(allowedMoves) {
+    let selectionInt;
+    
+    do {
+        let selection = prompt('Rock, Paper, Scissors?');
+        selectionInt = allowedMoves.findIndex(move => move.toLowerCase() === selection.toLowerCase());
+    }
+    while (selectionInt === -1)
+    
+    return selectionInt;
 }
 
+// Random move from computer
+function computerPlay(allowedMoves) {
+    return Math.floor(Math.random() * allowedMoves.length);
+}
+
+// Play a round
 function playRound(playerInt, computerInt) {
-    let roundResult = "???"
-    switch (winConditions[playerInt][computerInt]) {
-        case 'D':
-            return roundResult = `You drew :| ${moves[playerInt]} vs ${moves[computerInt]}`;
-        case 'L':
-            return roundResult = `You lost :( ${moves[playerInt]} vs ${moves[computerInt]}`;
-        case 'W':
-            return roundResult = `You won :) ${moves[playerInt]} vs ${moves[computerInt]}`;
-            
+    console.log(`${ moves[playerInt] } vs ${ moves[computerInt]}`); 
+    return winConditions[playerInt][computerInt];    
+}
+
+// Play a game
+function game(rounds = 5) {
+    let playerScore = 0;
+    let computerScore = 0;
+    
+    for (let i = 1; i <= rounds; i++) {
+        console.log("Round: " + i)
+        
+        roundResult = playRound(playerSelection(moves), computerPlay(moves));
+        switch (roundResult) {
+            case 'D':
+                console.log("You drew that round :|");
+                break;
+            case 'L':
+                console.log("You lost that round :(");
+                computerScore++;
+                break;
+            case 'W':
+                console.log("You won that round :)");
+                playerScore++;
+                break;
+        }
+    }
+    console.log("Your score: " + playerScore);
+    console.log("Computer score: " + computerScore);
+    
+    if(playerScore > computerScore) {
+        console.log("You won the game :)")
+    } 
+    else if (playerScore == computerScore) {
+        console.log("You drew the game :|")
+    }
+    else {
+        console.log("You lost the game :(")
     }
 }
 
-const playerSelection = 1; // Paper
-const computerSelection = computerPlay(moves);
-
-result = playRound(playerSelection, computerSelection);
-console.log(result);
+game(5);
