6efaa68cc049d0b8d673cdd68ab8c09e907409db
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-03-19 10:39:15 +0000

    add ability to work out result of a single round

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,12 +1,5 @@
 const moves = ['Rock', 'Paper', 'Scissors'];
 
-const allowedMoves = {
-    Rock:0,
-    Paper:1,
-    Scissors:2
-};
-console.log(allowedMoves);
-
 // 0 == Rock, 1 == Paper, 2 == Scissors
 // D == Draw, L == Loss, W == Win
 /* 
@@ -19,28 +12,26 @@ const winConditions = [
     ['W','D', 'L'],
     ['L','W', 'D']
 ];
-console.log(winConditions);
-// Paper Vs Rock
-console.log(winConditions[1][0]); // Win
-// Rock Vs Scissors
-console.log(winConditions[0][2]); // Win
-// Rock Vs Paper
-console.log(winConditions[0][1]); // Loss
 
 function computerPlay(moves) {
-    return moves[Math.floor(Math.random() * moves.length)];
+    return Math.floor(Math.random() * moves.length);
 }
 
-console.log(computerPlay(moves));
+function playRound(playerInt, computerInt) {
+    let roundResult = "???"
+    switch (winConditions[playerInt][computerInt]) {
+        case 'D':
+            return roundResult = `You drew :| ${moves[playerInt]} vs ${moves[computerInt]}`;
+        case 'L':
+            return roundResult = `You lost :( ${moves[playerInt]} vs ${moves[computerInt]}`;
+        case 'W':
+            return roundResult = `You won :) ${moves[playerInt]} vs ${moves[computerInt]}`;
+            
+    }
+}
 
-const playerSelection = "Rock";
+const playerSelection = 1; // Paper
 const computerSelection = computerPlay(moves);
-
-function playRound(playerSelection, computerSelection) {
-
-    
-}
 
 result = playRound(playerSelection, computerSelection);
 console.log(result);
-//console.log(playRound(playerSelection, computerSelection));
